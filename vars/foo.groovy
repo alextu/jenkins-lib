4c7f48c85062a129bcea8b65a3aa2292e91777f2
Alexis Tual <atual@gradle.com>
2024-09-24 16:18:32 +0200

    Add safety net

--- vars/foo.groovy
+++ vars/foo.groovy
@@ -1,4 +1,10 @@
-def bar() {
-    println 'Will try to find build scans'
-    findBuildScans()
+import org.jenkinsci.plugins.workflow.cps.CpsScript
+
+def bar(CpsScript script) {
+    if (script.getBinding().hasVariable('findBuildScans')) {
+        println 'Will try to find build scans'
+        findBuildScans()
+    } else {
+        println 'Method findBuildScans() is not present'
+    }
 }
