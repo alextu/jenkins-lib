507971c53cd2b60fc479fbbc598fe1840ace9358
Alexis Tual <atual@gradle.com>
2024-09-24 16:30:18 +0200

    Try something

--- vars/bar.groovy
+++ vars/bar.groovy
@@ -1,6 +1,6 @@
 import org.jenkinsci.plugins.workflow.cps.CpsScript
 
-def call(CpsScript script) {
+def call(CpsScript script = this) {
     if (script.getBinding().hasVariable('findBuildScans')) {
         println 'Will try to find build scans'
         findBuildScans()
