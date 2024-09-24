26054e894e3d5cc8f61a4646d2a8436d5f106ee6
Alexis Tual <atual@gradle.com>
2024-09-24 16:22:03 +0200

    Try moving to src

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -1,6 +1,6 @@
 import org.jenkinsci.plugins.workflow.cps.CpsScript
 
-def bar(CpsScript script) {
+def call(CpsScript script) {
     if (script.getBinding().hasVariable('findBuildScans')) {
         println 'Will try to find build scans'
         findBuildScans()
